7ecfb5bfeb357bd06fa34997dc623792a3d70f66
Ronald Segura <32435993+rsegurab@users.noreply.github.com>
2020-06-26 13:08:13 -0600

    CRUD ofertas + correcciones CRUD destinos

--- mantenimientoofertas.php
+++ mantenimientoofertas.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>Destinos Turísticos</title>
   <meta charset="utf-8">
@@ -8,96 +9,151 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-  
+
   <style>
-    .container-fluid { 
+    .container-fluid {
       position: absolute;
       width: 100%;
       height: 99%;
-      background-color: #5B8CA4; ; 
+      background-color: #5B8CA4;
+      ;
     }
-	h5{
-		text-align: center;
-	}
 
-    .centrado{
-        text-align: center;
-        margin-bottom: 100px;
+    h5 {
+      text-align: center;
     }
-	
-	
+
+    .centrado {
+      text-align: center;
+      margin-bottom: 100px;
+    }
   </style>
 
-<style type="text/css">
-  img {
-	  width: auto;
-	  height: auto;
-	  text-align: center;
-	  padding: 155px;
-	}
- </style>
+  <style type="text/css">
+    img {
+      width: auto;
+      height: auto;
+      text-align: center;
+      padding: 155px;
+    }
+  </style>
 </head>
+
 <body>
 
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <!-- Brand/logo -->
-  <a class="navbar-brand" href="#">
-  </a>
-  
-  <!-- Links -->
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="main.php">Principal</a>
-    </li>
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-        Destinos Turísticos
-      </a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="buscardestinos.php">Buscar destinos cercanos</a>
-        <a class="dropdown-item" href="sitiosmasvisitados.php">Sitios más visitados</a>
-        <a class="dropdown-item" href="ofertas.php">Ofertas</a>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <!-- Brand/logo -->
+    <a class="navbar-brand" href="#">
+    </a>
+
+    <!-- Links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="main.php">Principal</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+          Destinos Turísticos
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="buscardestinos.php">Buscar destinos cercanos</a>
+          <a class="dropdown-item" href="sitiosmasvisitados.php">Sitios más visitados</a>
+          <a class="dropdown-item" href="ofertas.php">Ofertas</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+          Mantenimiento
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="mantenimientodestinos.php">Administrar destinos</a>
+          <a class="dropdown-item" href="mantenimientoofertas.php">Administrar ofertas</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="mapadelsitio.php">Mapa del sitio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="creditos.php">Créditos</a>
+      </li>
+    </ul>
+  </nav>
+
+  <div class="container p-4">
+
+    <div class="row">
+
+      <div class="col-md-4">
+
+        <div class="card card-body">
+
+          <form action="crearoferta.php" method="POST">
+
+            <div class="form-group">
+
+              <h5> <b>Ingresar una nueva oferta para un sitio turístico</b> </h5>
+
+              <input type="submit" class="btn btn-primary btn-block" name="crearOferta" value="Crear oferta">
+
+            </div>
+
+          </form>
+
+        </div>
+
       </div>
-    </li>
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-        Mantenimiento
-      </a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="mantenimientodestinos.php">Administrar destinos</a>
-        <a class="dropdown-item" href="mantenimientoofertas.php">Administrar ofertas</a>
+
+      <div class="col-md-8">
+        <h5>Listado de destinaciones turísticas con ofertas disponibles</h5>
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Ubicación</th>
+              <th>Precio original</th>
+              <th>Porcentaje de descuento</th>
+              <th>Precio de oferta</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <?php
+
+            $servername = "163.178.107.10"; //La dirección del servidor MySQL
+            $username = "laboratorios"; //usuario del servidor
+            $password = "UCRSA.118"; //contraseña del servidor
+            $dbname = "proyectos_expertos_b16213_b77436"; //nombre de la base de datos que se va a utilizar
+
+            //Realiza la conexión a una base de datos, utilizando la dirección, usuario, contraseña y base de datos del servidor a utilizar
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // En caso que la conexión fallé, envía mensaje de conexión fallida.
+            if ($conn->connect_error) {
+              die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "select * from ofertasdestinos";
+            $result = mysqli_query($conn, $sql);
+
+            while ($row = mysqli_fetch_array($result)) { ?>
+              <tr>
+                <td> <?php echo $row['nombre_destino'] ?> </td>
+                <td> <?php echo $row['ubicacion_destino'] ?>  </td>
+                <td> <?php echo $row['precio_original'] ?> </td>
+                <td> <?php echo $row['porcentaje_descuento'] ?> % </td>
+                <td> <?php echo $row['precio_descontado'] ?> </td>
+                <td>
+                  <a href="modificaroferta.php?id=<?php echo $row['id_oferta'] ?>">Editar</a>
+                </td>
+              </tr>
+            <?php } ?>
+
+
+          </tbody>
+
+        </table>
+
       </div>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="mapadelsitio.php">Mapa del sitio</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="creditos.php">Créditos</a>
-    </li>
-  </ul>
-</nav>
 
-<div class="centrado">
-    
-</div>
+    </div>
 
-<div class="centrado">
-    
-</div>
-
-<div class="centrado">
-    
-</div>
-
-<div class="centrado">
-    <button type="button" class="btn btn-primary" onclick="window.location.href='crearoferta.php'">
-            Agregar oferta a destino turístico
-    </button>
-</div>
-
-<div class="centrado">
-    <button type="button" class="btn btn-primary" onclick="window.location.href='modificaroferta.php'">
-            Modificar oferta a destino turístico
-    </button>
-</div>
-
+  </div>
--- modificaroferta.php
+++ modificaroferta.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>Destinos Turísticos</title>
   <meta charset="utf-8">
@@ -8,98 +9,149 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-  
+
   <style>
-    .container-fluid { 
+    .container-fluid {
       position: absolute;
       width: 100%;
       height: 99%;
-      background-color: #5B8CA4; ; 
+      background-color: #5B8CA4;
+      ;
     }
-	h5{
-		text-align: center;
-	}
-	
+
+    h5 {
+      text-align: center;
+    }
   </style>
 
-<style type="text/css">
-  img {
-	  width: auto;
-	  height: auto;
-	  text-align: center;
-	  padding: 155px;
-	}
- </style>
+  <style type="text/css">
+    img {
+      width: auto;
+      height: auto;
+      text-align: center;
+      padding: 155px;
+    }
+  </style>
 </head>
+
 <body>
 
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <!-- Brand/logo -->
-  <a class="navbar-brand" href="#">
-  </a>
-  
-  <!-- Links -->
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="main.php">Principal</a>
-    </li>
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-        Destinos Turísticos
-      </a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="buscardestinos.php">Buscar destinos cercanos</a>
-        <a class="dropdown-item" href="sitiosmasvisitados.php">Sitios más visitados</a>
-        <a class="dropdown-item" href="ofertas.php">Ofertas</a>
-      </div>
-    </li>
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-        Mantenimiento
-      </a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="mantenimientodestinos.php">Administrar destinos</a>
-        <a class="dropdown-item" href="mantenimientoofertas.php">Administrar ofertas</a>
-      </div>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="mapadelsitio.php">Mapa del sitio</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="creditos.php">Créditos</a>
-    </li>
-  </ul>
-</nav>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <!-- Brand/logo -->
+    <a class="navbar-brand" href="#">
+    </a>
 
-<div>
-	<table style="width:70%">
-      <tr>
-		<td><b>Seleccione el destino del cual desea modificar una oferta:<b></td>
-		<td><select name="ofertaDestino">
-				<option value="C">Se llena dinamicamente</option>
-			</select></td>
-	  </tr>
-	  <tr>
-		<td><b>Porcentaje de descuento:<b></td>
-		<td><select name="descuentoDestino">
-                <option value="0"> 0%</option>
-				<option value="5"> 5%</option>
-				<option value="10">10%</option>
-				<option value="15">15%</option>
-				<option value="20">20%</option>
-				<option value="25">25%</option>
-				<option value="50">50%</option>
-				<option value="75">75%</option>
-			</select></td>
-	  </tr><tr>
-        <td><b>Ingrese la descripción de la oferta</b></td>
-        <td><input type="text" placeholder="Ingrese descripción" name="descripcionDestino" required></td>
-      </tr> 
-	</table>
-	<br>
-	<br>
-	<button type="button" class="btn btn-primary" onclick="window.location.href='modificaroferta.php'">
-		Modificar oferta
-	</button>
+    <!-- Links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="main.php">Principal</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+          Destinos Turísticos
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="buscardestinos.php">Buscar destinos cercanos</a>
+          <a class="dropdown-item" href="sitiosmasvisitados.php">Sitios más visitados</a>
+          <a class="dropdown-item" href="ofertas.php">Ofertas</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+          Mantenimiento
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="mantenimientodestinos.php">Administrar destinos</a>
+          <a class="dropdown-item" href="mantenimientoofertas.php">Administrar ofertas</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="mapadelsitio.php">Mapa del sitio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="creditos.php">Créditos</a>
+      </li>
+    </ul>
+  </nav>
 
-</div>
+  <?php
+
+  $servername = "163.178.107.10"; //La dirección del servidor MySQL
+  $username = "laboratorios"; //usuario del servidor
+  $password = "UCRSA.118"; //contraseña del servidor
+  $dbname = "proyectos_expertos_b16213_b77436"; //nombre de la base de datos que se va a utilizar
+
+  //Realiza la conexión a una base de datos, utilizando la dirección, usuario, contraseña y base de datos del servidor a utilizar
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // En caso que la conexión fallé, envía mensaje de conexión fallida.
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $query = "SELECT * FROM ofertasdestinos where id_oferta=$id";
+    $updateresult = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($updateresult) == 1) {
+      $row = mysqli_fetch_array($updateresult);
+
+      $name = $row['nombre_destino'];
+      $place = $row['ubicacion_destino'];
+      $originalPrice = $row['precio_original'];
+      
+    }
+
+
+    if (isset($_POST['modificarOferta'])) {
+      $id = $_GET['id'];
+      $discount = $_POST['porcentajeDescuento'];
+      $percentage = $discount / 100;
+      $calc = $originalPrice * $percentage;
+      $res = $originalPrice - $calc;
+
+
+      $sql1 = "SET SQL_SAFE_UPDATES=0;";
+      $sql2 = "UPDATE ofertasdestinos SET porcentaje_descuento=$discount,precio_descontado=$res WHERE id_oferta=$id;";
+      $sql3 = "SET SQL_SAFE_UPDATES=1;";
+      mysqli_query($conn, $sql1);
+      $res = mysqli_query($conn, $sql2);
+      mysqli_query($conn, $sql3);
+
+      if (!$res) {
+        die("Falló el query");
+      }
+
+      header("Location: mantenimientoofertas.php");
+    }
+  }
+
+
+  ?>
+
+  <div class="container p-4">
+    <div class="row">
+      <div class="col-md-4 mx-auto">
+        <div class="card card-body">
+          <form action="modificaroferta.php?id=<?php echo $_GET['id']; ?>" method="POST">
+            <div class="form-group">
+              <label>Ingrese el nuevo porcentaje de descuento que desea aplicar</label>
+              <select name="porcentajeDescuento" value>
+                <option value="" selected disabled hidden>Elija una opción</option>
+                <option value=0>0 %</option>
+                <option value=5>5 %</option>
+                <option value=10>10 %</option>
+                <option value=15>15 %</option>
+                <option value=25>25 %</option>
+                <option value=50>50 %</option>
+                <option value=75>75 %</option>
+              </select><br><br>
+
+              <button class="btn btn-primary btn-block" name="modificarOferta">Modificar oferta</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
